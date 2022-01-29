3cc7d91d00ccbc1b159d58ee8774dc94312ca30c
Losajhonny <losajhonny@gmail.com>
2022-01-29 16:05:35 -0600

    Laboratorio2

---
+++ Laboratorio2/src/analizadores/Nodo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package analizadores;
+
+/**
+ *
+ * @author Jhonny
+ */
+public class Nodo {
+    public int cont;
+    public String cad;
+    public int val;
+}
